56f0a59f3ce447fa6a67302c151bf6697db7f11e
Apolloscar <ohernandez4869@gmail.com>
2021-06-28 13:51:30 -0700

    df

--- Registration.js
+++ Registration.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const Datastore = require('nedb');
 
@@ -9,4 +10,17 @@ app.use(express.json({ limit: '1mb'}));
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+=======
+function register(phone, pass, pass2)
+{
+    if(pass != pass2)
+    {
+        alert("Passwords do not match");
+    }
+    else
+    {
+        alert(phone +"\n"+pass+"\n"+pass2);
+    }
+} 
+>>>>>>> parent of 8c2eb86 (no neww)
 
