2f549435189a25ab48e4352ce10bbcbb1d964e0c
Apolloscar <ohernandez4869@gmail.com>
2021-06-28 13:56:17 -0700

    df

--- Registration.js
+++ Registration.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-const express = require('express');
-const Datastore = require('nedb');
 
-const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb'}));
 
-const database = new Datastore('database.db');
-database.loadDatabase();
 
-=======
 function register(phone, pass, pass2)
 {
     if(pass != pass2)
@@ -22,5 +12,3 @@ function register(phone, pass, pass2)
         alert(phone +"\n"+pass+"\n"+pass2);
     }
 } 
->>>>>>> parent of 8c2eb86 (no neww)
-
