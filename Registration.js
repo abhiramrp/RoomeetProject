43f11948e8a3b11dbb5df1ce95e949ae24c0160b
Apolloscar <ohernandez4869@gmail.com>
2021-06-27 20:55:41 -0700

    sf

--- Registration.js
+++ Registration.js
@@ -1,11 +1,12 @@
-function register(phone, pass, pass2)
-{
-    if(pass != pass2)
-    {
-        alert("Passwords do not match");
-    }
-    else
-    {
-        alert(phone +"\n"+pass+"\n"+pass2);
-    }
-} 
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
