83c0fb1f64f4bd3f16ea605bf19ad81261948e9a
Abhiram Rishi Prattipati <64505707+abhiramrp@users.noreply.github.com>
2021-07-09 03:03:17 -0500

    First commit

---
+++ application.py
@@ -0,0 +1,26 @@
+import os
+import sqlite3
+
+from flask import Flask, render_template, request, session, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    session.clear()
+
+    if request.method == "POST":
+        request.form.
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        rpassword = request.form.get("rpassword")
+
+
