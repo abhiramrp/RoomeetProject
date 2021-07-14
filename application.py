4040511390e53a8b7698aafa3725b66b4e376b9c
Apolloscar <ohernandez4869@gmail.com>
2021-07-13 21:14:32 -0700

    back on track

--- application.py
+++ application.py
@@ -5,19 +5,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+app.secret_key = 'super secret key'
+
 @app.route("/")
 def index():
-<<<<<<< HEAD
     return render_template("home.html")
-=======
-    return render_template("profile.html")
->>>>>>> b3a0a32cfe171b7764d9fb4c5571f46dbe11fc62
 
-if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-
-    app.debug = True
-    app.run()
 
 @app.route("/registration", methods=["GET","POST"])
 def Registration():
@@ -29,10 +22,7 @@ def Registration():
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
-<<<<<<< HEAD
         rpassword = request.form.get("rpassword")
 
     return render_template("registration.html")
-=======
-        rpassword = request.form.get("rpassword")
->>>>>>> b3a0a32cfe171b7764d9fb4c5571f46dbe11fc62
+
